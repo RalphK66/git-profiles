c1d8e576b09436fc9e2067f2e3a8e7025c00a298
Ralph Kilian <rkilian1@my.bcit.ca>
2022-01-10 14:01:43 -0800

    deploy to heroku

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ import cors from 'cors'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 8080
-
 const app = express()
 
 app.use(express.json())
@@ -20,13 +18,14 @@ app.use(
     res.json({ token: process.env.ACCESS_TOKEN })
   })
 )
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
+
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT} `))
