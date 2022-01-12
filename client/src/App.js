25394e6447dc8d09ff40b6349dc4930e343a6eaa
Ralph Kilian <rkilian1@my.bcit.ca>
2022-01-11 19:25:07 -0800

    update not found route

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/user/:login" element={<User />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
   )
